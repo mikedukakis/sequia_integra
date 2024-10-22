73a734491f85e56db2a5c3cdd8a0aaa8b14eb79d
imf <imoyaf@gmail.com>
2024-10-22 14:42:58 +0200

    Create findById and findAll methods

--- src/main/java/sequia_integra/entity/rainfall/domain/RainfallService.java
+++ src/main/java/sequia_integra/entity/rainfall/domain/RainfallService.java
@@ -2,11 +2,20 @@ package sequia_integra.entity.rainfall.domain;
 
 import lombok.Data;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Data
 @Service
 public class RainfallService {
-    private final RainfallRepository;
+    private final RainfallRepository rainfallRepository;
 
+    public Mono<RainfallEntity> getRainfall(String id) {
+        return rainfallRepository.findById(id)
+                .switchIfEmpty(Mono.error(new RainfallNotFoundException("Rainfall not found with ID: " + id)));
+    }
 
+    public Flux<RainfallEntity> getAllRainfall() {
+        return rainfallRepository.findAll();
+    }
 }
