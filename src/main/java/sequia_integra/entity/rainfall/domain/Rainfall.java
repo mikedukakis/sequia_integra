ca6ede85d7f449a7a2072506c812855ef344ecdb
imf <imoyaf@gmail.com>
2024-10-22 12:27:25 +0200

    Create Rainfall attributes

--- src/main/java/sequia_integra/entity/rainfall/domain/Rainfall.java
+++ src/main/java/sequia_integra/entity/rainfall/domain/Rainfall.java
@@ -1,4 +1,22 @@
 package sequia_integra.entity.rainfall.domain;
 
-public class Precipitacio {
+import lombok.Data;
+
+@Data
+public class Rainfall {
+    private int any;
+    private double Precip_Acum_Gener;
+    private double Precip_Acum_Febrer;
+    private double Precip_Acum_Marc;
+    private double Precip_Acum_Abril;
+    private double Precip_Acum_Maig;
+    private double Precip_Acum_Juny;
+    private double Precip_Acum_Juliol;
+    private double Precip_Acum_Agost;
+    private double Precip_Acum_Setembre;
+    private double Precip_Acum_Octubre;
+    private double Precip_Acum_Novembre;
+    private double Precip_Acum_Desembre;
+
+
 }
