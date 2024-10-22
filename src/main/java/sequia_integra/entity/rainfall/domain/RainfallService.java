ce08459d3e1e224e27ff2fed3fe16fb7a6de3bb6
Dani Diaz <dan1@hotmail.es>
2024-10-22 18:31:17 +0200

    fix

--- src/main/java/sequia_integra/entity/rainfall/domain/RainfallService.java
+++ src/main/java/sequia_integra/entity/rainfall/domain/RainfallService.java
@@ -12,7 +12,7 @@ public class RainfallService {
 
     public Mono<RainfallEntity> getRainfall(int month, int year) {
         return rainfallRepository.findByMonthAndYear(month, year)
-                .switchIfEmpty(Mono.error(new RainfallNotFoundException("Rainfall not found with ID: " + id)));
+                .switchIfEmpty(Mono.error(new RuntimeException("Rainfall not found with ID: " )));
     }
 
     public Flux<RainfallEntity> getAllRainfall() {
