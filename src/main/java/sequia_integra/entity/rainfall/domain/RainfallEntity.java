3caab0bd3067b5b02344fffb794f9e1f3900863d
imf <imoyaf@gmail.com>
2024-10-22 18:39:11 +0200

    Add Field annotation to parametres

--- src/main/java/sequia_integra/entity/rainfall/domain/RainfallEntity.java
+++ src/main/java/sequia_integra/entity/rainfall/domain/RainfallEntity.java
@@ -3,14 +3,18 @@ package sequia_integra.entity.rainfall.domain;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Data
 @Document(collection = "rainfall")
 public class RainfallEntity {
     @Id
     private String id;
+    @Field
     private int Any;
+    @Field
     private int Mes;
+    @Field
     private double Precipitacions;
 
 }
